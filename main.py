eb4859517fd8a077b6de6ddb5fc5bc53353e45cf
Jeff Merckens <jeff.merckens@gmail.com>
2022-09-12 19:26:30 -0400

    Format with Black

--- main.py
+++ main.py
@@ -9,27 +9,37 @@ from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockBarsRequest, StockLatestBarRequest
 from alpaca.data.timeframe import TimeFrame
 from alpaca.trading.client import TradingClient
-from alpaca.trading.requests import AssetClass, AssetExchange, AssetStatus, GetAssetsRequest, MarketOrderRequest, OrderSide, OrderType, TimeInForce
+from alpaca.trading.requests import (
+    AssetClass,
+    AssetExchange,
+    AssetStatus,
+    GetAssetsRequest,
+    MarketOrderRequest,
+    OrderSide,
+    OrderType,
+    TimeInForce,
+)
 
 
 def sendNotification(message):
     """Send the string 'message' to my Discord channel and logs it via standard output."""
-    ID = os.environ.get('DISCORD_ID')
-    TOKEN = os.environ.get('DISCORD_TOKEN')
-    URL = f'https://discordapp.com/api/webhooks/{ID}/{TOKEN}'
-    payload = {'content': message,
-               'username': 'alpaca-algorithmic-trader',
-               'avatar_url': 'https://d1qb2nb5cznatu.cloudfront.net/startups/i/638844-fc9b06d417a209c9e53f71809af92091-medium_jpg.jpg?buster=1516408319'}
+    ID = os.environ.get("DISCORD_ID")
+    TOKEN = os.environ.get("DISCORD_TOKEN")
+    URL = f"https://discordapp.com/api/webhooks/{ID}/{TOKEN}"
+    payload = {
+        "content": message,
+        "username": "alpaca-algorithmic-trader",
+        "avatar_url": "https://d1qb2nb5cznatu.cloudfront.net/startups/i/638844-fc9b06d417a209c9e53f71809af92091-medium_jpg.jpg?buster=1516408319",
+    }
     print(message)
     return requests.post(URL, payload)
 
 
 def is_target_asset(asset):
     """Used to filter a list of assets to those of interest."""
-    return (asset.easy_to_borrow
-            and asset.marginable
-            and asset.shortable
-            and asset.tradable)
+    return (
+        asset.easy_to_borrow and asset.marginable and asset.shortable and asset.tradable
+    )
 
 
 def can_still_afford(symbols, bankroll):
@@ -38,7 +48,6 @@ def can_still_afford(symbols, bankroll):
     for symbol in symbols:
         bar_request = StockLatestBarRequest(symbol_or_symbols=symbol)
         bar = data_client.get_stock_latest_bar(bar_request)
-        print('In can_still_afford, the latest_bar was: ' + str(bar), flush=True)
 
         if float(bankroll) > bar.close:
             still_buyable.append(symbol)
@@ -59,7 +68,7 @@ def get_cheap_symbols(assets, low, high):
     [str]: a list of the stock symbols whose price
         falls within the given price range.
     """
-    print(f'looking through {len(assets)} assets...')
+    print(f"looking through {len(assets)} assets...")
     cheap_assets = []
     bar_request = StockLatestBarRequest(symbol_or_symbols=[x.symbol for x in assets])
     bars = data_client.get_stock_latest_bar(bar_request)
@@ -68,7 +77,7 @@ def get_cheap_symbols(assets, low, high):
         if bar.close > low and bar.close < high:
             cheap_assets.append(symbol)
         if index % 100 == 0:
-            print('100 assets queried...')
+            print("100 assets queried...")
 
     return cheap_assets
 
@@ -81,10 +90,10 @@ def get_SMAs(items):
         # print(index)
         if index + short_period <= len(items):
             shorter.append(
-                sum(items[index: (index + short_period - 1)]) / short_period)
+                sum(items[index : (index + short_period - 1)]) / short_period
+            )
         if index + long_period <= len(items):
-            longer.append(
-                sum(items[index: (index + long_period - 1)]) / long_period)
+            longer.append(sum(items[index : (index + long_period - 1)]) / long_period)
     return (shorter, longer)
 
 
@@ -103,7 +112,7 @@ def get_actionable_assets(cheap_assets):
         symbol_or_symbols=cheap_assets,
         start=datetime.now() - timedelta(days=210),
         limit=210,
-        timeframe=TimeFrame.Day
+        timeframe=TimeFrame.Day,
     )
     barSet = data_client.get_stock_bars(bars_request)
 
@@ -128,17 +137,15 @@ def main():
         start_time = datetime.now()
         global data_client
         data_client = StockHistoricalDataClient(
-            os.environ.get('APCA_API_KEY_ID'),
-            os.environ.get('APCA_API_SECRET_KEY')
+            os.environ.get("APCA_API_KEY_ID"), os.environ.get("APCA_API_SECRET_KEY")
         )
         trading_client = TradingClient(
-            os.environ.get('APCA_API_KEY_ID'),
-            os.environ.get('APCA_API_SECRET_KEY')
+            os.environ.get("APCA_API_KEY_ID"), os.environ.get("APCA_API_SECRET_KEY")
         )
 
         # Check if the market is open, if it isn't then don't do anything else
         market = trading_client.get_clock()
-        sendNotification(f'Is the market open? {market.is_open}!')
+        sendNotification(f"Is the market open? {market.is_open}!")
 
         if market.is_open:
             # Check if I own stocks and if I should sell them
@@ -152,10 +159,12 @@ def main():
                         qty=position.qty,
                         side=OrderSide.SELL,
                         type=OrderType.MARKET,
-                        time_in_force=TimeInForce.DAY
+                        time_in_force=TimeInForce.DAY,
                     )
                     order = trading_client.submit_order(order_request)
-                    sendNotification(f'A market SELL order was placed for: {order.symbol}')
+                    sendNotification(
+                        f"A market SELL order was placed for: {order.symbol}"
+                    )
 
             # Check if I have cash to buy stocks with
             account = trading_client.get_account()
@@ -163,19 +172,23 @@ def main():
                 nasdaq_asset_request = GetAssetsRequest(
                     status=AssetStatus.ACTIVE,
                     asset_class=AssetClass.US_EQUITY,
-                    exchange=AssetExchange.NASDAQ
+                    exchange=AssetExchange.NASDAQ,
                 )
                 nyse_asset_request = GetAssetsRequest(
                     status=AssetStatus.ACTIVE,
                     asset_class=AssetClass.US_EQUITY,
-                    exchange=AssetExchange.NYSE
+                    exchange=AssetExchange.NYSE,
                 )
-                assets = trading_client.get_all_assets(nasdaq_asset_request) + trading_client.get_all_assets(nyse_asset_request)
+                assets = trading_client.get_all_assets(
+                    nasdaq_asset_request
+                ) + trading_client.get_all_assets(nyse_asset_request)
 
                 target_assets = list(filter(is_target_asset, assets))
-                cheap_symbols = get_cheap_symbols(target_assets, 2, int(float(account.cash)))
+                cheap_symbols = get_cheap_symbols(
+                    target_assets, 2, int(float(account.cash))
+                )
                 buyable_symbols, sellable_symbols = get_actionable_assets(cheap_symbols)
-                sendNotification(f'Buyable symbols: {buyable_symbols}')
+                sendNotification(f"Buyable symbols: {buyable_symbols}")
 
                 while float(account.cash) > 5 and len(buyable_symbols) > 0:
                     order_request = MarketOrderRequest(
@@ -183,21 +196,23 @@ def main():
                         qty=1,
                         side=OrderSide.BUY,
                         type=OrderType.MARKET,
-                        time_in_force=TimeInForce.DAY
+                        time_in_force=TimeInForce.DAY,
                     )
                     order = trading_client.submit_order(order_request)
-                    sendNotification(f'A market BUY order was placed for: {order.symbol}')
+                    sendNotification(
+                        f"A market BUY order was placed for: {order.symbol}"
+                    )
                     # 1 minute; letting orders settle and cash update
                     time.sleep(60)
                     account = trading_client.get_account()
                     buyable_symbols = can_still_afford(buyable_symbols, account.cash)
 
-        sendNotification(f'This script took {datetime.now() - start_time}')
+        sendNotification(f"This script took {datetime.now() - start_time}")
     except Exception as e:
-        sendNotification(f'@everyone\nERROR: {e}')
+        sendNotification(f"@everyone\nERROR: {e}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     schedule.every().day.at("15:50").do(main)
 
     while True:
